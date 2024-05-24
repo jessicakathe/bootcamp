1743f54b52b41b8e55ef84a58ef968134b9c6271
Jessica Franco <jessica_kathe@hotmail.com>
2024-05-24 10:57:36 -0500

    Modulo_III: lab3

---
+++ Modulo_III/lab3/lab3.js
@@ -0,0 +1,26 @@
+
+/**  Comentario para documentación
+ *  @author Jessica Franco <jessicakathe@gmail.com>
+ *  @fileoverview This script use data types in JS
+ *  @license BSD 3-clause License
+*/
+
+// Declaration of variables
+let oneNumber = 2;
+let twoNumber = 3;
+let addition = 0;
+let substract = 0;
+let multiplication = 0;
+let division = 0;
+let modulus = 0;
+let name = ""; // funciona también con comillas simples ''
+
+addition = oneNumber + twoNumber
+substract = oneNumber - twoNumber
+multiplication = oneNumber * twoNumber
+division = oneNumber / twoNumber
+modulus = oneNumber % twoNumber
+incremento = oneNumber++
+decremento1 = oneNumber--
+// no se pueden representar fraccionarios, ni complejos, ni integrales
+
