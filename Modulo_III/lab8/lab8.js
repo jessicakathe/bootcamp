b7f347b1278179395f6e6cede4b8f679a74beaac
Jessica Franco <jessica_kathe@hotmail.com>
2024-05-24 10:58:54 -0500

    Modulo_III: lab8

---
+++ Modulo_III/lab8/lab8.js
@@ -0,0 +1,16 @@
+/**
+ *     @author Alejandro Romero <alejo8591@gmail.com>
+ *     @fileOverview This script use Function & Scopes in JS
+ *     @licence BSD 3-Clause License
+ */
+
+
+//declaration of variables
+// Function declaration
+
+function sayHelloOne(name)  {
+    return `Hello, ${name}`
+};
+
+
+console.log(`Function Declaration sayHello: ${sayHelloOne("Bootcamp 2024")}`);
