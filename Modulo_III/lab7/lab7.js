d618b8795e9587452ed72d443fc57ff3ce028458
Jessica Franco <jessica_kathe@hotmail.com>
2024-05-24 10:58:43 -0500

    Modulo_III: lab7

---
+++ Modulo_III/lab7/lab7.js
@@ -0,0 +1,21 @@
+/**
+ *     @author Alejandro Romero <alejo8591@gmail.com>
+ *     @fileOverview This script use Function & Scopes in JS
+ *     @licence BSD 3-Clause License
+ */
+
+
+//while loop
+let i = 0;
+while (i < 5) {
+    console.log(`i is ${i}`);
+    i++;
+}
+
+// do while
+
+i=0;
+do {
+    console.log(`i is ${i}`);
+    i++;    
+} while (i < 5);
