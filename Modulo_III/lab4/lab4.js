cc458adddb8f6fa647b39e61dd91692280f5aa99
Jessica Franco <jessica_kathe@hotmail.com>
2024-05-24 10:58:04 -0500

    Modulo_III: lab4

---
+++ Modulo_III/lab4/lab4.js
@@ -0,0 +1,14 @@
+
+/**  Comentario para documentación
+ *  @author Jessica Franco <jessicakathe@gmail.com>
+ *  @fileoverview This script use data types in JS
+ *  @license BSD 3-clause License
+*/
+
+//declaration of variables
+let x = 10; //Assignment operator
+console.log(`Value of x: ${x}`);
+
+x+=5 // Addition assigment operator
+console.log(`Value of x after addition assingment: ${x}`);
+
